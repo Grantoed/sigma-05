83236bcc075ae5ad7ca45794393282e4c5a8d054
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2023-08-24 13:48:22 +0300

    added loader, favicons & meta description

--- src/components/global/Loader/Loader.tsx
+++ src/components/global/Loader/Loader.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { useTheme } from "styled-components";
+import { Puff } from "react-loader-spinner";
 
 export const Loader = () => {
-  return <div>Loader</div>;
+  const theme = useTheme();
+
+  return (
+    <Puff
+      height="150"
+      width="150"
+      radius={1}
+      color={theme.colors.atc}
+      ariaLabel="puff-loading"
+      wrapperStyle={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "85vh",
+      }}
+      visible={true}
+    />
+  );
 };
